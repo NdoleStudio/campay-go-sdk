b1a8af58caf7f5b957cea805b3e3f18668efecc8
Acho Arnold <arnold@ndolestudio.com>
2021-12-20 23:13:06 +0200

    Add query qualifiers

--- callback.go
+++ callback.go
@@ -2,13 +2,13 @@ package campay
 
 // CallbackRequest is the notification request that is sent when a payment is made
 type CallbackRequest struct {
-	Status            string `json:"status"`
-	Reference         string `json:"reference"`
-	Amount            string `json:"amount"`
-	Currency          string `json:"currency"`
-	Operator          string `json:"operator"`
-	Code              string `json:"code"`
-	OperatorReference string `json:"operator_reference"`
-	Signature         string `json:"signature"`
-	ExternalReference string `json:"external_reference"`
+	Status            string `json:"status" query:"status"`
+	Reference         string `json:"reference" query:"reference"`
+	Amount            string `json:"amount" query:"amount"`
+	Currency          string `json:"currency" query:"currency"`
+	Operator          string `json:"operator" query:"operator"`
+	Code              string `json:"code" query:"code"`
+	OperatorReference string `json:"operator_reference" query:"operator_reference"`
+	Signature         string `json:"signature" query:"signature"`
+	ExternalReference string `json:"external_reference" query:"external_reference"`
 }
